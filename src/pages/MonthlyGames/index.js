1549736c4f6e698db1199ce2e0afcf2b7112a9c3
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2021-01-14 20:56:31 -0300

    feat: add loading and filter query

--- src/pages/MonthlyGames/index.js
+++ src/pages/MonthlyGames/index.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
-import { Text } from 'react-native';
+import { Text, ActivityIndicator } from 'react-native';
 
 import { MonthGames } from './monthlyGames.query';
 import { useFetch } from '../../service/fetch';
@@ -8,24 +8,36 @@ import { useFetch } from '../../service/fetch';
 
 const Wrapper = styled.View`
   background-color: #eee;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
 `;
 
 export default function MonthlyGames() {
-
+  const [monthGames, setMonthGames] = useState([]);
+  
   useEffect(() => {
-    async function anyNameFunction() {
+    async function getData() {
       const data = await useFetch(MonthGames);
-      console.log(data)
+      setMonthGames(data.monthGames);
     }
 
-    anyNameFunction();
-  
-  })
+      getData();
+  });
 
+  if (monthGames.length === 0) {
+    return (
+      <Wrapper>
+        <ActivityIndicator size={80} color="#0172CE" />
+        <Text>Searching for new games...</Text>
+      </Wrapper>
+    );
+  }
 
   return (
     <Wrapper>
-      <Text>Here when go</Text>
+      <Text>Here we go</Text>
     </Wrapper>
   )
 }
--- src/pages/MonthlyGames/monthlyGames.query.js
+++ src/pages/MonthlyGames/monthlyGames.query.js
@@ -2,7 +2,7 @@ import { gql } from 'graphql-request'
 
 export const MonthGames = gql`
   {
-    monthGames {
+    monthGames(sort: "published_at:desc", limit: 1) {
       id
       Release
       Title
